e9600539f8fb4ef4746ee7a32feceb271dc0976b
thorcamara <thorcamaraalthoff@gmail.com>
2023-09-10 22:06:02 -0300

    Added functions and EventListeners

--- script.js
+++ script.js
@@ -31,3 +31,47 @@ function startNewGame() {
     inputs.appendChild(input);
   }
 }
+
+function handleInput(e) {
+  const key = e.target.value.toLowerCase();
+  if (key.match(/^[a-z]+$/i) && !incorrectLetters.includes(`${key}`) && !correctLetters.includes(`${key}`)) {
+    if (word.includes(key)) {
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === key) {
+          inputs.querySelectorAll("input")[i].value += key;
+        }
+      }
+      correctLetters += key;
+    } else {
+      maxGuesses--;
+      incorrectLetters.push(`${key}`);
+      mistakes.innerText = incorrectLetters;
+    }
+  }
+
+  guessLeft.innerText = maxGuesses;
+  if (correctLetters.length === word.length) {
+    alert(`Congrats! You Found The Word ${word.toUpperCase()}`);
+    startNewGame();
+  } else if (maxGuesses < 1) {
+    alert("Game Over! You Don't Have Remaining Guesses!");
+    for (let i = 0; i < word.length; i++) {
+      inputs.querySelectorAll("input")[i].value = word[i];
+    }
+  }
+
+  typeInput.value = "";
+}
+
+function showHintElement() {
+  hintElement.style.display = "block";
+  hintElement.style.opacity = "1";
+}
+
+resetBtn.addEventListener("click", startNewGame);
+hintBtn.addEventListener("click", showHintElement);
+typeInput.addEventListener("input", handleInput);
+inputs.addEventListener("click", () => typeInput.focus());
+document.addEventListener("keydown", () => typeInput.focus());
+
+startNewGame();
